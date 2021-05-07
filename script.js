36010052b122518ab634381dee7f809611b764de
Yen Le <yenlt6@onemount.com>
2021-05-08 00:40:34 +0700

    bt buoi 2

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("RUN SCRIPT");
+document.getElementsByClassName("title-section").stype.fontWeight="900"
+document.getElementsByClassName("title-section").stype.fontSize="32px"
+document.getElementById("OMG-footer").style.backgroundColor="#0984e3"
+document.getElementsByClassName("footer-textarea").innerText="© 2021 One Mount Group, JSC."
+document.getElementsByTagName("img").src="onhousing.png"
+// Đổi style của tiêu đề Our companies thành chữ in đậm và kích thước 32px
+// Gợi ý: Sử dụng .style.fontWeight và .style.fontSize
+
+// Đổi màu nền của phần footer thành màu #0984e3
+// Gợi ý: Sử dụng .style.backgroundColor
+
+// Đổi đoạn text ở footer từ "© 2020 One Mount Group, JSC." thành "© 2021 One Mount Group, JSC."
+// Gợi ý: Sử dụng .innerText
+
+// Đổi ảnh đầu tiên thành ảnh onhousing.png
+// Gợi ý: Sử dụng .src
